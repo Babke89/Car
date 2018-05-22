fe9570e79c3219ae2f5801a14309b935460dcd99
Stefan <stefan.babic@gmail.com>
2018-05-22 20:24:44 +0200

    Prva inicijalizacija

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+        System.out.println("Maksimalna brzina je: " + myCar.maxSpeed);
+        System.out.println(myCar.condition);
+        System.out.println(myCar.currentPrice);
+        System.out.println(myCar.license);
+        System.out.println(myCar.minSpeed);
+        System.out.println(myCar.weight);
+        
+        Car companyCar = new Car(150, 30, 2000, false, "ERF-456", 'B', 3500, 6001);
+        System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed);
+    }
+
+}
